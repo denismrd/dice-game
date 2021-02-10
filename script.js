b2a8ad9b6107d28f3cc9aff04cfabc196392a3d3
denismrd <71873001+denismrd@users.noreply.github.com>
2021-02-10 20:57:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,20 @@ let hold,
     roll,
     player1,
     player2,
-    score,
-    points,
     p1_score,
-    p1_points,
+    p1_current,
     p2_score,
-    p2_points,
+    p2_current,
+    p1,
+    p2,
+    wintext,
+    win,
+    howbut,
+    help,
+    helpbox,
     current,
-    img;
+    img,
+    wonbox;
 hold = document.querySelector(".hold");
 roll = document.querySelector(".roll-dice");
 player1 = document.querySelector(".player1");
@@ -19,19 +25,46 @@ p1_current = document.querySelector(".player1 .current .point");
 p2_score = document.querySelector(".player2 .score .point");
 p2_current = document.querySelector(".player2 .current .point");
 img = document.querySelector(".controls .dice-img");
-let wintext = document.querySelector(".won .box .text");
-let win = document.querySelector(".won");
+wintext = document.querySelector(".won .box .text");
+win = document.querySelector(".won");
+howbut = document.querySelector(".how");
+help = document.querySelector(".help");
+helpbox = document.querySelector(".help-box");
+wonbox = document.querySelector(".won .box");
+function start() {
+    p1_score.textContent = 0;
+    p2_score.textContent = 0;
+    p1_current.textContent = 0;
+    p2_current.textContent = 0;
+    p1 = {
+        ps: 0,
+        cs: 0,
+    };
+    p2 = {
+        ps: 0,
+        cs: 0,
+    };
+    current = p1;
+    player1.className = "player player1 select";
+    player2.className = "player player2";
+}
+start();
+document.querySelector(".wonnew").addEventListener("click", start);
+document.querySelector(".new").addEventListener("click", start);
+howbut.addEventListener("click", function () {
+    howbut.style.animation = "none";
+    help.classList.add("show-overlay");
+    helpbox.style.transform = "translateY(0)";
+});
+help.addEventListener("click", function () {
+    help.classList.remove("show-overlay");
+    helpbox.style.transform = "translateY(-100vh)";
+});
+document.querySelector(".wonnew").addEventListener("click", function () {
+    win.classList.remove("show-overlay");
+    wonbox.style.transform = "scale(0)";
+});
 
-p1 = {
-    ps: 0,
-    cs: 0,
-};
-p2 = {
-    ps: 0,
-    cs: 0,
-};
-current = p1;
-let i = 0;
 hold.addEventListener("click", switchPlayer);
 roll.addEventListener("click", rollHandler);
 
@@ -41,8 +74,10 @@ function switchPlayer() {
         p1_score.textContent = p1.ps + "";
         p1_current.textContent = 0;
         if (p1.ps >= 100) {
-            win.style.display = "flex";
+            win.classList.add("show-overlay");
+            wonbox.style.transform = "scale(1)";
             wintext.textContent = "PLAYER-1 won the game";
+
             return;
         }
         p1.cs = 0;
@@ -54,7 +89,7 @@ function switchPlayer() {
         p2_score.textContent = p2.ps + "";
         p2_current.textContent = 0;
         if (p2.ps >= 100) {
-            win.style.display = "flex";
+            win.style.transform = "scale(1)";
             wintext.textContent = "PLAYER-2 won the game";
             return;
         }
