1e8060541a5a1dbff2ebc6b2bc6c17100481de2a
denismrd <71873001+denismrd@users.noreply.github.com>
2021-02-14 16:47:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,129 +1,105 @@
-let hold,
-    roll,
-    player1,
-    player2,
-    p1_score,
-    p1_current,
-    p2_score,
-    p2_current,
-    p1,
-    p2,
-    wintext,
-    win,
-    howbut,
-    help,
-    helpbox,
-    current,
-    img,
-    wonbox;
-hold = document.querySelector(".hold");
-roll = document.querySelector(".roll-dice");
-player1 = document.querySelector(".player1");
-player2 = document.querySelector(".player2");
-p1_score = document.querySelector(".player1 .score .point");
-p1_current = document.querySelector(".player1 .current .point");
-p2_score = document.querySelector(".player2 .score .point");
-p2_current = document.querySelector(".player2 .current .point");
-img = document.querySelector(".controls .dice-img");
-wintext = document.querySelector(".won .box .text");
-win = document.querySelector(".won");
-howbut = document.querySelector(".how");
-help = document.querySelector(".help");
-helpbox = document.querySelector(".help-box");
-wonbox = document.querySelector(".won .box");
-function start() {
+const player1 = document.querySelector('.player1');
+const player2 = document.querySelector('.player2');
+const p1_score = document.querySelector('.player1 .score .point');
+const p1_current = document.querySelector('.player1 .current .point');
+const p2_score = document.querySelector('.player2 .score .point');
+const p2_current = document.querySelector('.player2 .current .point');
+const hold = document.querySelector('.hold');
+const roll = document.querySelector('.roll-dice');
+const img = document.querySelector('.controls .dice-img');
+const wintext = document.querySelector('.won .box .text');
+const win = document.querySelector('.won');
+const howbut = document.querySelector('.how');
+const help = document.querySelector('.help');
+const helpbox = document.querySelector('.help-box');
+const wonbox = document.querySelector('.won .box');
+let player_data, num, current;
+
+//Set up new game
+function new_game() {
     p1_score.textContent = 0;
     p2_score.textContent = 0;
     p1_current.textContent = 0;
     p2_current.textContent = 0;
-    p1 = {
-        ps: 0,
-        cs: 0,
+    player_data = {
+        p1: {
+            ps: 0,
+            cs: 0
+        },
+        p2: {
+            ps: 0,
+            cs: 0
+        }
     };
-    p2 = {
-        ps: 0,
-        cs: 0,
-    };
-    current = p1;
-    player1.className = "player player1 select";
-    player2.className = "player player2";
+    num = 1;
+    current = player_data[`p${num}`];
+    player1.className = 'player player1 select';
+    player2.className = 'player player2';
 }
-start();
-document.querySelector(".wonnew").addEventListener("click", start);
-document.querySelector(".new").addEventListener("click", start);
-howbut.addEventListener("click", function () {
-    howbut.style.animation = "none";
-    help.classList.add("show-overlay");
-    helpbox.style.transform = "translateY(0)";
-});
-help.addEventListener("click", function () {
-    help.classList.remove("show-overlay");
-    helpbox.style.transform = "translateY(-100vh)";
-});
-document.querySelector(".wonnew").addEventListener("click", function () {
-    win.classList.remove("show-overlay");
-    wonbox.style.transform = "scale(0)";
+new_game();
+
+//When user clicks 'New game'
+document.querySelector('.wonnew').addEventListener('click', new_game);
+document.querySelector('.new').addEventListener('click', new_game);
+
+//Show 'How to play' box
+howbut.addEventListener('click', function () {
+    howbut.style.animation = 'none';
+    help.classList.add('show-overlay');
+    helpbox.style.transform = 'translateY(0)';
 });
 
-hold.addEventListener("click", switchPlayer);
-roll.addEventListener("click", rollHandler);
+//Hide 'how to play' box when user clicks somewhere
+help.addEventListener('click', function () {
+    help.classList.remove('show-overlay');
+    helpbox.style.transform = 'translateY(-100vh)';
+});
+
+//Hide 'Won game' box when user clicks 'New game'
+document.querySelector('.wonnew').addEventListener('click', function () {
+    win.classList.remove('show-overlay');
+    wonbox.style.transform = 'scale(0)';
+});
+
+hold.addEventListener('click', switchPlayer);
+roll.addEventListener('click', rollHandler);
 
 function switchPlayer() {
-    if (current === p1) {
-        p1.ps += p1.cs;
-        p1_score.textContent = p1.ps + "";
-        p1_current.textContent = 0;
-        if (p1.ps >= 100) {
-            win.classList.add("show-overlay");
-            wonbox.style.transform = "scale(1)";
-            wintext.textContent = "PLAYER-1 won the game";
-            return;
-        }
-        p1.cs = 0;
-        current = p2;
-        player1.classList.remove("select");
-        player2.classList.add("select");
-    } else {
-        p2.ps += p2.cs;
-        p2_score.textContent = p2.ps + "";
-        p2_current.textContent = 0;
-        if (p2.ps >= 100) {
-            win.classList.add("show-overlay");
-            wonbox.style.transform = "scale(1)";
-            wintext.textContent = "PLAYER-2 won the game";
-            return;
-        }
-        p2.cs = 0;
-        current = p1;
-        player2.classList.remove("select");
-        player1.classList.add("select");
+    current.ps += current.cs;
+    eval(`p${num}_score`).textContent = current.ps;
+    current.cs = 0;
+    eval(`p${num}_current`).textContent = current.cs;
+
+    //Show 'won game' box when score reaches the threshhold
+    if (current.ps >= 100) {
+        win.classList.add('show-overlay');
+        wonbox.style.transform = 'scale(1)';
+        wintext.textContent = `PLAYER-${num} won the game`;
+        return;
     }
-}
-function changeCurrent() {
-    if (current === p1) {
-        p1_current.textContent = current.cs + "";
-    } else {
-        p2_current.textContent = current.cs + "";
-    }
+
+    eval(`player${num}`).classList.remove('select');
+    num = num === 1 ? 2 : 1;
+    current = player_data[`p${num}`];
+    eval(`player${num}`).classList.add('select');
 }
 function rollHandler() {
     let rand = Math.floor(Math.random() * 6 + 1);
-    if (rand === 1) {
-        changeImage(1);
-        current.cs = 0;
-        changeCurrent();
-        switchPlayer();
-    } else {
-        changeImage(rand);
-        current.cs += rand;
-        changeCurrent();
-    }
-}
-function changeImage(n) {
-    let cls = "zoom" + n;
+
+    //Show dice image based on random number
+    let cls = 'zoom' + rand;
     if (img.classList.contains(cls)) {
-        img.classList.toggle("zoomt" + n);
+        img.classList.toggle('zoomt' + rand);
     } else {
         img.className = `dice-img ${cls}`;
+    }
+
+    //Handle when dice hits 1
+    if (rand === 1) {
+        current.cs = 0;
+        switchPlayer();
+    } else {
+        current.cs += rand;
+        eval(`p${num}_current`).textContent = current.cs;
     }
 }
